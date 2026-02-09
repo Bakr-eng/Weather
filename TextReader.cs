69c1982fd0b17d71eb9e80209605871968f931a9
seventh <mikael.smile@hotmail.se>
2026-02-09 13:58:48 +0100

    TextReader Base Class
    Textreader without functionality to be worked on.

---
+++ TextReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather
+{
+    internal class TextReader
+    {
+        static async Task ReadFile(string path)
+        {
+
+        }
+    }
+}
